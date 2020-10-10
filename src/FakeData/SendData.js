ac9ee1f0114a9403174c6e2b24ceca9f5c920edd
Zahangir-Kabir <zahangir.ithome@gmail.com>
2020-10-11 01:09:34 +0600

    update

---
+++ src/FakeData/SendData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import volunteerData from './fakeData.json'
+
+const SendData = () => {
+    const sendData = () => {
+        fetch('http://localhost:5000/addEvent',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(volunteerData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+    return (
+        <div>
+            <button onClick={sendData}>Send Data</button>
+        </div>
+    );
+};
+
+export default SendData;
